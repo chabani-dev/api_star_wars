a2d235de0c90910a2c4307589d67c2a98ac77d27
chabani-dev <zerkachabani2021@gmail.com>
2022-12-08 12:15:50 +0100

    new modified

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from "./logo.svg";
+import logo from "./Star-wars.png";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Peoples from "./components/Peoples";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
+      <img src={logo} alt="" width="100" />
       <Routes>
-        <Route path="/peoles" element={<Peoples />} />
+        <Route path="/peoles/:id" element={<Peoples />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Peoples.js
+++ src/components/Peoples.js
@@ -1,10 +1,50 @@
-import React from "react";
 import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 
 export default function Peoples() {
-  return (
-    <div>
-      <h1>The Star Wars API</h1>
-    </div>
-  );
+  const [people, setPeoplesArray] = useState(null);
+  const [error, setError] = useState(null);
+  const [loaded, setLoaded] = useState(false);
+  const params = useParams();
+  const id = params.id;
+  const url = `https://swapi.dev/api/films/${id}`;
+
+  useEffect(() => {
+    axios
+      .get(url)
+      // on traite la promesse reçue grâce à axios
+      .then((response) => {
+        console.log(response);
+        setPeoplesArray(response.data);
+        setLoaded(true);
+      })
+      .catch((err) => {
+        console.log(err);
+        setError(err);
+        setLoaded(true);
+      });
+  }, [url]);
+
+  // si la réponse n'est pas traitée (en cours)
+  if (!loaded) {
+    return <div>En cours de chargement...</div>;
+    // si la réponse est traitée mais comporte des erreurs
+  } else if (error) {
+    return <div>{error.message}</div>;
+    // si tout va bien
+  } else {
+    return (
+      <div className="col-9">
+        <h1 className="text-center">{"Titre :" + people.title}</h1>
+        <div className="d-flex justify-content-center">
+          <p>{" Nombre d'episode : " + people.episode_id} </p>
+          <p>{"Rampe d'ouverture :" + people.opening_crawl}</p>
+          <p>{"Director: " + people.director}</p>
+          <p>{"Producteur :" + people.producer}</p>
+          <p>{"Date de réalisation  :" + people.release_date}</p>
+        </div>
+      </div>
+    );
+  }
 }
